7fd0eb1d68dfdff5fcf9cc82b597859fd08b21cb
jerome <jvgonzalvo@up.edu.ph>
2017-09-09 14:57:35 +0800

    Simplified fizbuzz.py by removing boolean flag.

--- fizzbuzz_py/fizzbuzz.py
+++ fizzbuzz_py/fizzbuzz.py
@@ -5,16 +5,12 @@ class FizzBuzz:
             self.substitution_map = {}
 
         def get_value_of(self, number):
-            result = ''
-            is_multiple_of_a_previous_number = False
+            result = []
             for key, substitute in self.substitution_map.items():
                 if number % key == 0:
-                    if is_multiple_of_a_previous_number:
-                        result += ' '
-                    result += substitute
-                    is_multiple_of_a_previous_number = True
-            if is_multiple_of_a_previous_number:
-                return result
+                    result.append(substitute)
+            if result:
+                return ' '.join(result)
             return number
 
         def add_to_substitution_map(self, number, substitute):
